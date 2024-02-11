8d197194cf73b8e8025dfd8dfe3b5ce68b728de5
Ramsheed KK <117103380+ramsheedrahman@users.noreply.github.com>
2024-02-11 14:55:12 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ connectDB()
 const __filename=fileURLToPath(import.meta.url)
 const __dirname=path.dirname(__filename);
 //middelwares
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'client','build')));
 app.use(cors());
 app.use(express.json({limit:'10mb'}));
 app.use(morgan("dev"));
@@ -24,7 +24,7 @@ app.use("/category",categoryRoute);
 app.use("/product",productsRoute);
 
 app.use('*',function(req,res){
-  res.sendFile(path.join(__dirname, './client/build/index.html'))
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html''))
 })
 
 const PORT = process.env.PORT || 8000;
